f565cfe63a78b2d89855abc27fcad8d061758934
paras594 <parasarora594@gmail.com>
2025-12-26 19:30:58 +0530

    add relative imports, react hot toast

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "react-hot-toast";
 import { AuthContextType } from "../providers/auth-provider";
 import PWABadge from "../shared/components/pwa-badge/PWABadge";
 
@@ -15,6 +16,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <>
       <Outlet />
 
+      <Toaster position="top-center" reverseOrder={false} />
       <PWABadge />
       <TanStackRouterDevtools />
       <ReactQueryDevtools buttonPosition="bottom-right" />
